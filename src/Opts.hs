c5c64de79eefa5176b2842d3c70fccfbc2af02d5
Dimitrios Vytiniotis <dimitris@microsoft.com>
2014-07-02 11:20:20 +0100

    Deleting legacy parsers and removing --new-src-format option.

--- src/Opts.hs
+++ src/Opts.hs
@@ -43,7 +43,6 @@ data DynFlag =
   | CSrcPathPosix FilePath
   | CSrcPathNative FilePath
   | Debug
-  | NewSrcFormat
   | StdoutDump
   | BoundsCheck
 
@@ -138,12 +137,8 @@ options =
      , Option []        ["autolut"]             (NoArg AutoLUT)       "automatically convert function to use LUTs"
      , Option []        ["pipeline"]            (NoArg Pipeline)      "pipeline standalone computations"
 
-
      , Option []        ["native-mitigators"]   (NoArg NativeMitigators) "use native mitigators instead of source-based"
      , Option []        ["no-lut-hashing"]      (NoArg NoLUTHashing)     "do not hash lut generation"
-
-
-     , Option []        ["new-src-format"]      (NoArg NewSrcFormat)  "new source format"
 
      , Option []        ["dummy-thread"]        (NoArg DummyThread)   "generate dummy thread when pipelining"
      , Option []        ["ddump-to-file"]       (NoArg DumpToFile)    "dump to a file"
