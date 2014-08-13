ef7e6c94ca883f97311dd9bbd0a1dfbe3a23caf6
Bozidar <bozidar@users.noreply.github.com>
2014-08-13 15:28:05 +0100

    Fixed bug in pkt counters

--- code/WiFi/mac/mac_2threads.c
+++ code/WiFi/mac/mac_2threads.c
@@ -227,7 +227,7 @@ BOOLEAN __stdcall go_thread_tx(void * pParam)
 	const int headerSizeInBytes = 3;
 	unsigned char * headerBuf;
 	unsigned char * payloadBuf;
-	unsigned int * payloadBuf16;
+	uint16 * payloadBuf16;
 	unsigned int payloadSize;
 
 
@@ -236,7 +236,7 @@ BOOLEAN __stdcall go_thread_tx(void * pParam)
 	buf_ctx_tx.mem_input_buf = (void *)malloc(maxInSize);
 	headerBuf = (unsigned char*)buf_ctx_tx.mem_input_buf;
 	payloadBuf = (unsigned char*)buf_ctx_tx.mem_input_buf + headerSizeInBytes;
-	payloadBuf16 = (unsigned int *)payloadBuf;
+	payloadBuf16 = (uint16 *)payloadBuf;
 
 	if (params_tx->outType == TY_SORA)
 	{
@@ -298,7 +298,7 @@ BOOLEAN __stdcall go_thread_tx(void * pParam)
 	else
 	{
 		// SORA output
-		unsigned int pktCnt = 0;
+		uint16 pktCnt = 0;
 
 		while (1) 
 		{
@@ -427,7 +427,7 @@ BOOLEAN __stdcall go_thread_rx(void * pParam)
 		unsigned char lastMod;
 		unsigned char lastEnc;
 		unsigned int lastLen;
-		unsigned int pktCnt = 0xFFFF;
+		uint16 pktCnt = 0xFFFF;
 		unsigned long cntOk = 0;
 		unsigned long cntError = 0;
 		unsigned long cntMiss = 0;
@@ -451,8 +451,8 @@ BOOLEAN __stdcall go_thread_rx(void * pParam)
 
 			unsigned int lengthInBytes = buf_ctx_rx.total_out / 8 - 5;
 			unsigned char * payload = (unsigned char *) buf_ctx_rx.mem_output_buf;
-			unsigned int * payload16 = (unsigned int *)buf_ctx_rx.mem_output_buf;
-			unsigned int pc = payload16[0];
+			uint16 * payload16 = (uint16 *)buf_ctx_rx.mem_output_buf;
+			uint16 pc = payload16[0];
 			bool pktOK;
 
 			lastCRC = payload[lengthInBytes];
@@ -483,8 +483,8 @@ BOOLEAN __stdcall go_thread_rx(void * pParam)
 
 			if (lastPrint + printDelay < cntOk + cntError + cntMiss)
 			{
-				printf("Last packet: crc=%d, mod=%d, enc=%d, len=%d, buf_len=%d, lastGap=%ld\n",
-					lastCRC, lastMod, lastEnc, lastLen, lengthInBytes, lastGap);
+				printf("Last packet: cnt=%d, crc=%d, mod=%d, enc=%d, len=%d, buf_len=%d, lastGap=%ld\n",
+					pc, lastCRC, lastMod, lastEnc, lastLen, lengthInBytes, lastGap);
 				printf("OK: %ld, Error: %ld, Miss: %ld\n", cntOk, cntError, cntMiss);
 				fflush(stdout);
 				lastPrint = cntOk + cntError + cntMiss;
