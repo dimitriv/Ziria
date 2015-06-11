69ab0561c8507adf0d6b5a6fc3ea72920f26f82e
smolkaj <smolka@cs.cornell.edu>
2015-06-11 10:15:01 +0100

    Ziria checkpoint + use linux lineending

--- src/Atomix/AutomataModel.hs
+++ src/Atomix/AutomataModel.hs
@@ -4,26 +4,24 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 
 import Data.Graph.Inductive.Graph  ( Node )
-import qualified Data.Graph.Inductive.Graph  as G 
+import qualified Data.Graph.Inductive.Graph  as G
+import Data.Graph.Inductive.PatriciaTree as G
 
-import Data.Graph.Inductive.PatriciaTree
 import Control.Exception
-
 import Control.Monad.Reader
-import Control.Monad.State 
+import Control.Monad.State
 
 import qualified GenSym as GS
-
-
-import AstExpr       -- BasicTypes/ 
+import AstExpr       -- BasicTypes/
 import AstComp       -- BasicTypes/
-import AstUnlabelled 
+import AstUnlabelled
+import Opts
 
-
+-- type Node = int
 type Chan = EId
 type ChanEnv = [(EId,Chan)]
 
-data NodeLabel 
+data NodeLabel
   = NodeLabel { node_id   :: Node
               , node_kind :: NodeKind
               }
@@ -35,7 +33,7 @@ data NodeKind
            , action_env  :: ChanEnv
            }
 
-  | Loop { action_times :: Maybe Int }
+  | Loop { loop_times :: Maybe Int }
 
   | Branch { branch_in   :: Set Chan
            , branch_code :: Exp
@@ -47,80 +45,106 @@ data Automaton
   = Automaton { automaton_init  :: Node
               , automaton_final :: Set Node }
 
-
-concatAutomata :: Automaton -> Automaton -> GraphM Automaton
-concatAutomata = error "IMPLEMENT ME!" 
-
-newtype ZirGraph = ZirGraph { graph :: Gr NodeLabel () }
+type ZirGraph = Gr NodeLabel ()
 
 data ZirEnv = ZirEnv { chan_binds  :: ChanEnv
-                     , chan_gensym :: GS.Sym 
+                     , chan_gensym :: GS.Sym
                      }
 
 type GraphM a = StateT ZirGraph (ReaderT ZirEnv IO) a
+
+mkActionNode :: Set Chan -> Set Chan -> Exp -> ChanEnv -> GraphM Node
+mkActionNode inp outp code env = do
+  let act = Action inp outp code env
+  g <- get
+  let [node] = G.newNodes 1 g
+  let lnode = (node, NodeLabel node act)
+  put (G.insNode lnode g)
+  return node
+
 
 extendChan :: (EId,Chan) -> GraphM a -> GraphM a
 extendChan (x,c) = local add_bnd
   where add_bnd (ZirEnv binds sym) = ZirEnv ((x,c):binds) sym
 
-lookupChan :: EId -> GraphM Chan
-lookupChan x = do 
-   env <- ask
+--lookupChan :: EId -> GraphM Chan
+--lookupChan x = do
+--   env <- ask
+--   case lookup x (chan_binds env) of
+--      Nothing   -> fail ("Automata generation: unbound variable " ++ (show x))
+--      Just chan -> return chan
+
+-- TODO: use proper exception
+lookupChan :: ZirEnv -> EId -> Chan
+lookupChan env x =
    case lookup x (chan_binds env) of
-      Nothing   -> fail ("Automata generation: unbound variable " ++ (show x))
-      Just chan -> return chan
+      Nothing   -> assert False undefined
+      Just chan -> chan
 
 freshChan :: EId -> GraphM Chan
-freshChan x = do 
+freshChan x = do
   u <- liftIO . GS.genSymStr =<< asks chan_gensym
   return $ x { uniqId = MkUniq u }
 
 runGraphM :: GraphM a -> IO (a,ZirGraph)
 runGraphM m = do
    new_sym <- GS.initGenSym "automata"
-   runReaderT (runStateT m (ZirGraph G.empty)) (ZirEnv [] new_sym)
+   runReaderT (runStateT m (G.empty)) (ZirEnv [] new_sym)
 
 {-------------------- Translation --------------------}
+
+concatAutomata :: Automaton -> Automaton -> GraphM Automaton
+concatAutomata a1 a2 = do
+    let edges = map (\x -> (x, automaton_init a2, ())) (Set.toList $ automaton_final a1)
+    g <- get
+    put (G.insEdges edges g)
+    return $ Automaton (automaton_init a1) (automaton_final a2)
 
 data Channels = Channels { in_chan   :: Chan
                          , out_chan  :: Chan
                          , ctrl_chan :: Maybe Chan }
 
 mkAtomGraph :: DynFlags -> Channels -> Comp -> GraphM Automaton
-mkAtomGraph dfs comp = go (unComp comp) 
-  where 
+mkAtomGraph dfs chans comp = go chans (unComp comp)
+  where
     loc = compLoc comp
     go :: Channels -> Comp0 -> GraphM Automaton
 
     go chans (BindMany c1 []) = mkAtomGraph dfs chans c1
 
-    go chans (BindMany c1 ((x1,c2):xs_s)) = do
+    go chans (BindMany c1 ((x1,c2):cs)) = do
       ctrl <- freshChan x1
       a1 <- mkAtomGraph dfs (chans { ctrl_chan = Just ctrl }) c1
-      a2 <- extendChan (x1,ctrl) $ 
-            go chans (BindMany c2 xs_cs)
+      a2 <- extendChan (x1,ctrl) $
+            go chans (BindMany c2 cs)
       concatAutomata a1 a2
 
     go chans (Seq c1 c2) = do
       a1 <- mkAtomGraph dfs (chans { ctrl_chan = Nothing }) c1
-      a2 <- mkAtomGraph dfs chans c2 
-      concatAutomata a1 a2 
+      a2 <- mkAtomGraph dfs chans c2
+      concatAutomata a1 a2
 
 
     go chans (Emit e) = do
-        
- 
+      env <- ask
+      let inp = Set.map (lookupChan env) (exprFVs' False e)
+      node <- mkActionNode inp Set.empty e (chan_binds env)
+      return $ Automaton node (Set.singleton node)
 
 
 
-        
-         
+
+
+
+
+
+
 
 
 -- runStateT (ZirGraph G.empty) (runReaderT m [])
 -- type GraphM expr var a = Env var Uniq -> Graph expr var -> (a, Graph expr var)
 
--- extend :: (var,Uniq) -> GraphM expr var a -> GraphM expr var a 
+-- extend :: (var,Uniq) -> GraphM expr var a -> GraphM expr var a
 -- lookup :: var -> GraphM expr var Uniq
 
 -- put :: ..
@@ -136,20 +160,20 @@ mkAtomGraph dfs comp = go (unComp comp)
 
 
 -- translate (Seq c1 (x,c2)) = do
---    a1 <- translate c1 
+--    a1 <- translate c1
 --    a2 <- extend x (translate c2)
 --    return (merge_automata a1 a2)
 -- }
 
--- translate (If e c1 c2) = do 
+-- translate (If e c1 c2) = do
 --    a1 <- translate c1
 --    a2 <- translate c2
 --    let vsins = varsOf e
 --    c <- mkControl vsins e (start_node a1) (start_node a2)
-   
---    return 
 
-  
+--    return
+
+
 
 
 
