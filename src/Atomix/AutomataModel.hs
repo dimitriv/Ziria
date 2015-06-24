94c05461840ee07486161105360412e8b9b932b9
Steffen Smolka <smolka@cs.cornell.edu>
2015-06-24 12:26:15 +0100

    hardcore hacking with Ayal

--- src/Atomix/AutomataModel.hs
+++ src/Atomix/AutomataModel.hs
@@ -75,45 +75,6 @@ class Atom a where
 
 
 
-type MarkingM nid = State (Set nid)
-mark :: Ord nid => nid ->  MarkingM nid ()
-mark nid = do modify (Set.insert nid)
-
-isMarked :: Ord nid => nid -> MarkingM nid Bool
-isMarked nid = do
-  marks <- get
-  return (Set.member nid marks)
-
-
-fuseActions :: Ord nid => Automaton atom nid -> Automaton atom nid
-fuseActions auto = auto { auto_graph = fused_graph }
-  where
-    fused_graph = fst $ runState (markAndFuse (auto_start auto) (auto_graph auto)) Set.empty
-
-    markAndFuse :: Ord nid => nid -> NodeMap atom nid -> MarkingM nid (NodeMap atom nid)
-    markAndFuse nid nmap = do
-      marked <- isMarked nid
-      if marked then return nmap else do
-        mark nid
-        fuse (fromJust $ Map.lookup nid nmap) nmap
-
-    fuse :: Ord nid => Node atom nid -> NodeMap atom nid -> MarkingM nid (NodeMap atom nid)
-    fuse (Node _ Done) nmap = return nmap
-    fuse (Node _ (Loop b)) nmap = markAndFuse b nmap
-    fuse (Node _ (AutomataModel.Branch _ b1 b2 _)) nmap = do
-      nmap <- markAndFuse b1 nmap
-      markAndFuse b2 nmap
-    fuse (Node nid (Action atoms next)) nmap = do
-      nmap <- markAndFuse next nmap
-      case fromJust (Map.lookup next nmap) of
-        Node _ Done -> return nmap
-        Node _ (Loop _) -> return nmap
-        Node _ (AutomataModel.Branch {}) -> return nmap
-        Node nid' (Action atoms' next') ->
-          let new_node = Node nid (Action (atoms++atoms') next')
-              new_nmap = Map.insert nid new_node (Map.delete nid' nmap)
-          in return new_nmap
-
 
 
 nextNid :: Automaton atom Int -> Int
@@ -220,15 +181,150 @@ mkAutomaton dfs chans comp k = go (unComp comp)
 
 
 
-zipAutomata :: Automaton e Int -> Automaton e Int -> Automaton e Int -> CompM a (Automaton e Int)
-zipAutomata a1 a2 k = fail "not implemented"
+-- Mapping Automata Labels
+
+map_ids :: Ord nid1 => Ord nid2 => (nid1 -> nid2) -> Automaton e nid1 -> Automaton e nid2
+map_ids map_id a = a { auto_graph = new_graph, auto_start = new_start }
+ where
+    new_start = map_id (auto_start a)
+    new_graph =  Map.mapKeys map_id $ Map.map map_node $ auto_graph a
+    map_node (Node nid nkind) = Node (map_id nid) (map_nkind nkind)
+    map_nkind Done =  Done
+    map_nkind (Loop bodyId) = Loop (map_id bodyId)
+    map_nkind (Action atoms nextId) = Action atoms (map_id nextId)
+    map_nkind (AutomataModel.Branch x left right is_while) = 
+      AutomataModel.Branch x (map_id left) (map_id right) is_while
+
+tuple_ids :: Automaton e Int -> Automaton e (Int,Int)
+tuple_ids = map_ids (\nid -> (nid,0))
+
+untuple_ids :: Automaton e ((Int,Int),(Int,Int)) -> Automaton e Int
+untuple_ids a = map_ids (\nid -> fromJust $ Map.lookup nid untuple_map) a
+  where
+    (_, untuple_map) = Map.foldWithKey f (0, Map.empty) (auto_graph a)
+    f nid _ (counter, nid_map) = (counter+1, Map.insert nid counter nid_map)
 
 
 
---instance Show (NodeKind atom nid) where
---  show (NodeLabel _ (Action watom)) =
---    let show_wires = List.intercalate "," . map show
---    in show_wires (wires_in watom) ++ "/" ++ show_wires (wires_out watom)
---  show (NodeLabel _ (AutomataModel.Branch win _)) = show win ++ "/DQ"
---  show (NodeLabel _ (StaticLoop n _)) = "/DQ="
+-- marking automata nodes
+
+type MarkingM nid = State (Set nid)
+mark :: Ord nid => nid ->  MarkingM nid ()
+mark nid = do modify (Set.insert nid)
+
+isMarked :: Ord nid => nid -> MarkingM nid Bool
+isMarked nid = do
+  marks <- get
+  return (Set.member nid marks)
+
+
+
+-- fusing actions sequences in automata
+
+fuseActions :: Ord nid => Automaton atom nid -> Automaton atom nid
+fuseActions auto = auto { auto_graph = fused_graph }
+  where
+    fused_graph = fst $ runState (markAndFuse (auto_start auto) (auto_graph auto)) Set.empty
+
+    markAndFuse :: Ord nid => nid -> NodeMap atom nid -> MarkingM nid (NodeMap atom nid)
+    markAndFuse nid nmap = do
+      marked <- isMarked nid
+      if marked then return nmap else do
+        mark nid
+        fuse (fromJust $ Map.lookup nid nmap) nmap
+
+    fuse :: Ord nid => Node atom nid -> NodeMap atom nid -> MarkingM nid (NodeMap atom nid)
+    fuse (Node _ Done) nmap = return nmap
+    fuse (Node _ (Loop b)) nmap = markAndFuse b nmap
+    fuse (Node _ (AutomataModel.Branch _ b1 b2 _)) nmap = do
+      nmap <- markAndFuse b1 nmap
+      markAndFuse b2 nmap
+    fuse (Node nid (Action atoms next)) nmap = do
+      nmap <- markAndFuse next nmap
+      case fromJust (Map.lookup next nmap) of
+        Node _ Done -> return nmap
+        Node _ (Loop _) -> return nmap
+        Node _ (AutomataModel.Branch {}) -> return nmap
+        Node nid' (Action atoms' next') ->
+          let new_node = Node nid (Action (atoms++atoms') next')
+              new_nmap = Map.insert nid new_node (Map.delete nid' nmap)
+          in return new_nmap
+
+
+
+-- Zipping Automata
+
+zipAutomata :: Automaton e Int -> Automaton e Int -> Automaton e Int -> Automaton e Int
+zipAutomata a1 a2 k = untuple_ids $ prod_a 
+  where
+    prod_a = Automaton prod_nmap (auto_inchan a1) (auto_outchan a2) prod_start
+    prod_start = (auto_start a1', auto_start a2')
+    a1' = tuple_ids (fuseActions a1)
+    a2' = tuple_ids (fuseActions a2)
+    nmap1 = auto_graph a1'
+    nmap2 = auto_graph a2'
+    prod_nmap = go (auto_start a1') (auto_start a2') Map.empty
+
+    go nid1 nid2 prod_nmap = 
+      case Map.lookup (nid1,nid2) prod_nmap of
+        Nothing -> go' (fromJust $ Map.lookup nid1 nmap1) (fromJust $ Map.lookup nid2 nmap2) prod_nmap
+        Just _ -> prod_nmap -- TODO: what should we do here?
+
+    go' (Node id1 Done) (Node id2 _) prod_nmap =
+      let prod_nid = (id1,id2)
+      in Map.insert prod_nid (Node prod_nid Done)
+
+    go' (Node id1 _) (Node id2 Done) prod_nmap =
+      let prod_nid = (id1,id2)
+      in Map.insert prod_nid (Node prod_nid Done)
+
+    go' (Node id1 (AutomataModel.Branch x l r w)) (Node id2 _) prod_nmap =
+      let prod_nid = (id1,id2)
+          prod_nkind = AutomataModel.Branch x (l,id2) (r,id2) w
+          prod_node = Node prod_nid prod_nkind
+      in go r id2 $ go l id2 $ Map.insert prod_nid prod_node prod_nmap
+
+    go' (Node id1 _) (Node id2 (AutomataModel.Branch x l r w)) prod_nmap =
+      let prod_nid = (id1,id2)
+          prod_nkind = AutomataModel.Branch x (id1,l) (id1,r) w
+          prod_node = Node prod_nid prod_nkind
+      in go id1 r $ go id1 l $ Map.insert prod_nid prod_node prod_nmap
+
+    go' (Node id1 (Action watoms1 next1)) (Node id2 (Action watoms2 next2)) =
+      let out_card1 = count_writes watoms1 (auto_outchan a1)
+          in_card2 = count_reades watoms2 (auto_inchan a2)
+          card = min out_card1 in_card2
+          split watoms1 card (auto_outchan a1)
+
+
+
+
+type MarkingM nid = State (Set nid)
+mark :: Ord nid => nid ->  MarkingM nid ()
+mark nid = do modify (Set.insert nid)
+
+isMarked :: Ord nid => nid -> MarkingM nid Bool
+isMarked nid = do
+  marks <- get
+  return (Set.member nid marks)
+
+
+data NodeKind atom nid
+  = Action { atoms :: [WiredAtom atom]
+           , next  :: nid
+           }
+  | Branch { branch_var   :: Chan -- If we read True we go to branch_true, otherwise to branch_false
+           , branch_true  :: nid
+           , branch_false :: nid
+           , is_while     :: Bool -- Is this a while loop?
+           }
+  | Loop { loop_body :: nid } -- Infinite loop. Only transformers may (and must!) contain one of these.
+  | Done
+
+instance Show (NodeKind atom nid) where
+  show (NodeLabel _ (Action watom)) =
+    let show_wires = List.intercalate "," . map show
+    in show_wires (wires_in watom) ++ "/" ++ show_wires (wires_out watom)
+  show (NodeLabel _ (AutomataModel.Branch win _)) = show win ++ "/DQ"
+  show (NodeLabel _ (StaticLoop n _)) = "/DQ="
 
