f3d9c9c4a83cfd18fdb5cda22fd9e26995700f8f
Steffen Smolka <smolka@cs.cornell.edu>
2015-06-25 14:30:06 +0100

    wifi mockup skeleton

--- src/Atomix/AtomInstantiation.hs
+++ src/Atomix/AtomInstantiation.hs
@@ -5,6 +5,7 @@ import AutomataModel
 import AstExpr (Ty (..), EId, GName (..), GArgTy (..), ArgTy, Uniq (..), MutKind (..))
 
 import Data.Maybe
+import qualified Data.List as List
 
 
 
@@ -63,16 +64,82 @@ funAppToWiredAtom e mb = WiredAtom ins outs (FunFun f)
 -- For simplicity, we will ignore types for now
 ty = TBit
 
-mkApp func args = MkExp { unExp = expr, expLoc = undefined, expInfo = ()}
-  where expr = ExpApp { expAppFun = func, expAppArgs = args}
+mkComp c = MkComp c undefined ()
+mkExp e = MkExp e undefined ()
+
+mkApp func args = mkExp $ ExpApp { expAppFun = func, expAppArgs = args}
 
 mkFunc name nins nouts = MkName { name = name
-                               , uniqId = MkUniq name
-                               , nameTyp = funty
-                               , nameLoc = undefined
-                               , nameMut = undefined }
+                                , uniqId = MkUniq name
+                                , nameTyp = funty
+                                , nameLoc = undefined
+                                , nameMut = undefined }
   where
     funty = TArrow argtys ty
     argtys = [ GArgTy { argty_ty = ty, argty_mut = Imm } | _ <- [1..nins] ] ++
              [ GArgTy { argty_ty = ty, argty_mut = Mut} | _ <- [2..nouts] ]
+
+mkPar c1 c2 = mkComp (Par undefined c1 c2)
+
+mkChan :: CompM () Var
+mkChan = undefined
+
+ziria_map func = do
+  x <- mkChan
+  return $ mkComp $ Bind (Just x) (mkComp $ Take1 ty) (mkComp $ Return $ mkApp func [x])
+
+
+
+
+
+
+-- Mockup of Wifi Pipeline
+
+wifi :: CompM () (Comp () ())
+wifi = do
+
+  -- channels for control data
+  det <- mkChan
+  params <- mkChan
+  h <- mkChan
+
+  -- DetectSTS
+  removeDC <- ziria_map (mkFunc "removeDC" 1 1)
+  let cca = undefined
+  let detectSTS = mkPar removeDC cca
+
+  -- LTS
+  let lts = undefined
+
+  -- middle pipeline
+  let dataSymbol = undefined
+  let fft = undefined
+  let channelEqualization = undefined
+  let pilotTrack = undefined
+  let getData = undefined
+  let middlePipeline = List.foldr mkPar getData [dataSymbol, fft, channelEqualization, pilotTrack]
+
+  -- DecodePLCP
+  let demodBPSK = undefined
+  let deinterleave = undefined
+  let decode = undefined
+  let parseHeader = undefined
+  let decodePLCP = List.foldr mkPar parseHeader [middlePipeline, demodBPSK, deinterleave, decode]
+
+  -- Decode
+  let demod = undefined
+  let deinterleave = undefined
+  let decode = undefined
+  let descramble = undefined
+  let decodePipeline = List.foldr mkPar descramble [middlePipeline, demod, deinterleave, decode]
+
+  -- overall receiver
+  let mkBind (mbvar, pipeline) acc = mkComp $ Bind mbvar pipeline acc
+
+  return $ List.foldr mkBind decodePipeline [ (Just det, detectSTS)
+                                            , (Just params, lts)
+                                            , (Just h, decodePLCP) ]
+
+
+
 
