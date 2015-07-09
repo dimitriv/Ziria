76f17d243ef56a7e828d54819cdd9297b32503be
Dimitrios Vytiniotis <dimitris@microsoft.com>
2015-07-09 10:35:38 +0100

    Better error reporting.

--- src/BasicTypes/AstComp.hs
+++ src/BasicTypes/AstComp.hs
@@ -344,7 +344,8 @@ mkHOCompSubst fprms fargs = go fprms fargs ([],[],[])
 
     rev_acc (x,y,z) = (reverse x, reverse y, reverse z)
 
-    to_comp_nm nm = updNameTy nm (callArg (error "mkHOCompSubst") id (nameTyp nm))
+    to_comp_nm nm = updNameTy nm $ 
+                    callArg (error "mkHOCompSubst") id (nameTyp nm)
 
 
 checkCAArgMut :: -- Function argument types (expected)
