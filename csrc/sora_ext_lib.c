bb08a9cd53d3d80fc7cc2a178f9bc8c801b5e5df
Bozidar <bozidar@microsoft.com>
2015-09-21 23:25:16 +0100

    Fixed stdint types on Linux

--- csrc/sora_ext_lib.c
+++ csrc/sora_ext_lib.c
@@ -16,6 +16,7 @@
    See the Apache Version 2.0 License for specific language governing
    permissions and limitations under the License.
 */
+#include <stdint.h>
 #include "types.h"
 #include "string.h"
 #include <assert.h>
